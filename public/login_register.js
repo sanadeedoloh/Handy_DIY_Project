f11b9e3402b085ce4ecc88c53123fe0ee0e6b6c1
sanadeedoloh <62239239+sanadeedoloh@users.noreply.github.com>
2021-09-24 11:46:59 +0700

    login file and other file

---
+++ public/login_register.js
@@ -0,0 +1,169 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjw9hEux7cBFUaTM5-V2fsCmjPbpmj9yo",
+  authDomain: "diyproject-93b1f.firebaseapp.com",
+  projectId: "diyproject-93b1f",
+  storageBucket: "diyproject-93b1f.appspot.com",
+  messagingSenderId: "25915915585",
+  appId: "1:25915915585:web:89f1e518c905db9158f5c0",
+  measurementId: "G-XRGSS0L8WP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth()
+const database = firebase.database()
+
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+  full_name = document.getElementById('full_name').value
+  confirm_password = document.getElementById('confirm_password').value
+  
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password is Outta Line!!')
+    return
+    // Don't continue running the code
+  }
+  if (validate_field(full_name) == false || validate_field(confirm_password) == false ) {
+    alert('One or More Extra Fields is Outta Line!!')
+    return
+  }
+ 
+
+  // Move on with Auth
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+
+      // Create User data
+      var user_data = {
+        email: email,
+        full_name: full_name,
+        confirm_password: confirm_password,
+        
+        last_login: Date.now()
+      }
+      if (user) {
+        // User is signed in.
+        console.log(user);
+        window.location.href = 'login.html';
+    } 
+
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+
+      // DOne
+      alert('User Created!!')
+      
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+
+      alert(error_message)
+    })
+}
+
+// Set up our login function
+function login() {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password ไม่ถูกต้อง')
+    return
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+  
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+
+      // Create User data
+      var user_data = {
+        last_login: Date.now()
+        
+      }
+      if (user) {
+        // User is signed in.
+        console.log(user);
+        window.location.href = '../profile-blog.html';
+    } 
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+
+      // DOne
+      alert('User Logged In!!')
+      
+
+    })
+
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+
+      alert(error_message)
+    })
+}
+
+
+
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Email is not good
+    return false
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function SignOut() {
+  auth.SignOut();
+  alert("Sign Out");
+  window.location.href = 'login.html';
+
+}
+
+
+function validate_field(field) {
+  if (field == null) {
+    return false
+  }
+
+  if (field.length <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
---
+++ public/show_data.js
@@ -0,0 +1,44 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCjw9hEux7cBFUaTM5-V2fsCmjPbpmj9yo",
+    authDomain: "diyproject-93b1f.firebaseapp.com",
+    projectId: "diyproject-93b1f",
+    storageBucket: "diyproject-93b1f.appspot.com",
+    messagingSenderId: "25915915585",
+    appId: "1:25915915585:web:89f1e518c905db9158f5c0",
+    measurementId: "G-XRGSS0L8WP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+  
+  // Set up our register function
+  function loadUser() {
+   
+    
+ 
+   
+   
+  
+    
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
